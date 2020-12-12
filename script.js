ceffe4c226b9b38ee8395a7e0f79bb841ea5d526
Michael Wall <mikesterwallace8@gmail.com>
2020-12-11 20:06:32 -0500

    added time

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//Time variables using "moment.js"
+let cDate = moment().format('Do');
+let cMonth = moment().format('MMMM');
+let cDay = moment().format('dddd');
+
+//HTML elements Variables
+let pDay = $('#currentDay');
+let Hours = $('.description');
+
+//All saveBtns
+let saveBtn = $('.saveBtn');
